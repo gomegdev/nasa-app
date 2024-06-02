a33f7ed002970a85a40a818eb4b5375d30b60039
Johnrome Pangon <gomegxxiv@gmail.com>
2024-06-03 05:21:19 +0800

    3rd commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Main = ({ data }) => {
   return (
-    <div className="flex flex-col items-center justify-center p-4">
+    <div className="flex w-full flex-col items-center justify-center">
       {data.media_type === "image" ? (
         <img
           src={data.url}
           alt={data.title || "NASA Image of the Day"}
-          className="min-h-screen min-w-max object-cover"
+          className="min-h-screen object-cover"
         />
       ) : (
         <p>No image available for today.</p>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Sidebar = ({ handleToggleModal, data }) => {
   return (
-    <div className="fixed inset-0 z-10 flex flex-col shadow-[0_0_40px_1px_#030615]">
+    <div className="fixed inset-0 z-10 flex flex-col shadow-[0_0_40px_1px_#030615] transition md:relative md:inset-[unset] md:max-h-screen md:w-[30%]">
       <div
         className="absolute inset-0 bg-[#030615] opacity-60 md:fixed"
         onClick={handleToggleModal}
       ></div>
-      <div className="relative z-[15] ml-auto flex h-full w-[90%] max-w-[800px] flex-col gap-4 overflow-y-scroll bg-[#030615] p-4">
+      <div className="relative z-[15] ml-auto flex h-full min-h-screen w-[90%] max-w-[800px] flex-col gap-4 overflow-y-scroll bg-black p-4 md:min-h-screen md:w-full">
         <h2 className="text-2xl font-extralight capitalize">{data?.title}</h2>
         <div className="flex-1 space-y-2">
           <p className="text-base font-extralight uppercase tracking-widest opacity-70">
